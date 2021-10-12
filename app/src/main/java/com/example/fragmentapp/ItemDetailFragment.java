c8974c30f8465eb3036ec3c872c047380eaa7196
Ariiel08 <ariel_ortega98@hotmail.com>
2021-10-11 20:37:48 -0400

    x

--- app/src/main/java/com/example/fragmentapp/ItemDetailFragment.java
+++ app/src/main/java/com/example/fragmentapp/ItemDetailFragment.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Switch;
 import android.widget.TextView;
 import androidx.fragment.app.Fragment;
 import com.example.fragmentapp.placeholder.PlaceholderContent;
@@ -40,13 +41,15 @@ public class ItemDetailFragment extends Fragment {
         TextView name = view.findViewById(R.id.tvName);
         TextView version = view.findViewById(R.id.tvVersion);
         TextView releaseDate = view.findViewById(R.id.tvReleaseDate);
-        TextView supported = view.findViewById(R.id.tvSupported);
+        Switch supported = view.findViewById(R.id.swSupported);
         TextView internalCode = view.findViewById(R.id.tvInternalCode);
 
         details.setText(element.getDetails());
         name.setText("Name: "+element.getName());
         version.setText("Version: "+element.getVersionNumber());
         internalCode.setText("Internal Code: "+element.getInternalCodeName());
+        supported.setChecked(element.isSupported());
+        releaseDate.setText("Release date: "+element.getReleaseDate().toString());
 
         Button btnLink = view.findViewById(R.id.button);
 
